fdab1537cd71a8a712a9c2ef7b0a1fb5bb0697b3
Trần Lê Duy <tranleduy1233@gmail.com>
2017-03-09 16:25:51 +0700

    fix nested function

--- app/src/main/assets/code_sample/dos_get_time.pas
+++ app/src/main/assets/code_sample/dos_get_time.pas
@@ -1,20 +1,20 @@
 program test_time;
 var
-  a,b,c,d : integer;
+    a, b, c, d : integer;
 
-	function print(w:integer):string;
-	var
-	  s : string;
-	begin
-	  Str(w,s);
-	  if w<10 then
-	   result :='0'+s
-	  else
-	   result := s;
-	end;
+function print(w: integer): string;
+var
+    s : string;
+begin
+    Str(w,s);
+    if w < 10 then
+        result := '0' + s
+    else
+        result := s;
+end;
 
 begin
-  GetTime(a,b,c,d);
-  WriteLn('Current time');
-  WriteLn(print(a),':',print(b),':',print(c));
+    GetTime(a,b,c,d);
+    WriteLn('Current time');
+    WriteLn(print(a),':',print(b),':',print(c));
 end.
