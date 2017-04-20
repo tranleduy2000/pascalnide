522d38b0cce7e8f780e9c981bac5b26487978d52
Trần Lê Duy <tranleduy1233@gmail.com>
2017-04-20 14:58:48 +0700

    update copyright

--- app/src/main/java/com/duy/pascal/frontend/sample/CodeHolder.java
+++ app/src/main/java/com/duy/pascal/frontend/sample/CodeHolder.java
@@ -1,3 +1,19 @@
+/*
+ *  Copyright 2017 Tran Le Duy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.duy.pascal.frontend.sample;
 
 import android.support.v7.widget.RecyclerView;
@@ -6,7 +22,6 @@ import android.widget.TextView;
 
 import com.duy.pascal.frontend.R;
 import com.duy.pascal.frontend.view.code_view.CodeView;
-import com.ramotion.foldingcell.FoldingCell;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -26,10 +41,7 @@ class CodeHolder extends RecyclerView.ViewHolder {
     View btnCopy;
     @BindView(R.id.code_view)
     CodeView codeView;
-    @BindView(R.id.code_view_small)
-    CodeView codeViewSmall;
-    @BindView(R.id.folding_cell)
-    FoldingCell foldingCell;
+
 
     public CodeHolder(View itemView) {
         super(itemView);
