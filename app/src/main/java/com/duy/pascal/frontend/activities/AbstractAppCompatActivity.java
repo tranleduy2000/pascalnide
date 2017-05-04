6f265795d956bc1a3ab76e9316ebc3e0fb643d4d
Trần Lê Duy <tranleduy1233@gmail.com>
2017-05-04 13:07:19 +0700

    disable toast

--- app/src/main/java/com/duy/pascal/frontend/activities/AbstractAppCompatActivity.java
+++ app/src/main/java/com/duy/pascal/frontend/activities/AbstractAppCompatActivity.java
@@ -34,7 +34,6 @@ import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.duy.pascal.frontend.BuildConfig;
 import com.duy.pascal.frontend.Dlog;
