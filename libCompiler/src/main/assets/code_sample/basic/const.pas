b7381a604adf0933e5332ecbdf09a7fdc26ca71c
Trần Lê Duy <tranleduy1233@gmail.com>
2017-05-27 09:55:28 +0700

    add code sample

--- libCompiler/src/main/assets/code_sample/basic/const.pas
+++ libCompiler/src/main/assets/code_sample/basic/const.pas
@@ -1,10 +1,17 @@
 program constant;
 const
-    a = 10;
-    s = 'pascal';
-    c = 'char';
+    a = 10; //integer constant
+    s = 'pascal'; //string constant
+    c = 'c'; //char constant
+    d : string[10] = 'asdhashdhasdhashd'; //string constant
+    e: integer = 100; //integer constant
+    f: longint = 12112312; //longint constant
+    g: real = 1.2; //real constant
+    i = 1.2;//real constant
+    k = true; //boolean constant
+    m : boolean = true and false; //boolean constant
+
 begin
-    writeln(a, ' ', s, ' ', c);
     readln;
 end.
 {if you want to improve this code, please send code to me
