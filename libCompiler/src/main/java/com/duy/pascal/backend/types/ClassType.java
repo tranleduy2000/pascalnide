72672e4892b46d670be8ee492e2aad253766c6a6
Trần Lê Duy <tranleduy1233@gmail.com>
2017-06-10 11:35:15 +0700

    update grade build

--- libCompiler/src/main/java/com/duy/pascal/backend/types/ClassType.java
+++ libCompiler/src/main/java/com/duy/pascal/backend/types/ClassType.java
@@ -1,8 +1,15 @@
 package com.duy.pascal.backend.types;
 
+import com.duy.pascal.backend.ast.FunctionDeclaration;
+
 public class ClassType extends CustomType {
+
+    private FunctionDeclaration constructor, destructor;
+
     @Override
     public String getEntityType() {
         return "class";
     }
+
+
 }
