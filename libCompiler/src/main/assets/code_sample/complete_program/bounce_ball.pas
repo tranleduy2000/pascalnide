5983c4389bbdc43aae13f0e6eb8792c7864d7ec0
Trần Lê Duy <tranleduy1233@gmail.com>
2017-06-18 20:05:18 +0700

    update sample

--- libCompiler/src/main/assets/code_sample/complete_program/bounce_ball.pas
+++ libCompiler/src/main/assets/code_sample/complete_program/bounce_ball.pas
@@ -2,32 +2,37 @@ program bounceBalls;
 
 { --------
   Bouncing balls demo for Pascal N-IDE
-  by: Mʀ Bee -- @pak_lebah
-  June 13, 2017
+  by: Mr Bee -- @pak_lebah
 }
 
 uses
   CRT, Graph;
 
 type
-    // ball structure
-
   TBall = record
-    bc, br : integer;
-    bx, by : integer;
-    dx, dy : integer;
-    mx, my : integer;
-    nx, ny : integer;
-    gl, gu : boolean;
+    bc,br: integer;
+    bx,by: integer;
+    dx,dy: integer;
+    mx,my: integer;
+    nx,ny: integer;
+    gl,gu: boolean;
   end;
 
 const
   nBall = 9; // number of balls
 
 var
-  Balls : array[1..nBall] of TBall;
+  Balls: array[1..nBall] of TBall;
 
-procedure MoveBall(var aBall : TBall);
+function randomColor: integer;
+var
+  cs: android_graphics_Color;
+begin
+  randomColor := cs.rgb(random(224)+32,
+                         random(224)+32,random(224)+32);
+end;
+
+procedure MoveBall(var aBall: TBall);
 begin
   with aBall do
   begin
@@ -60,83 +65,82 @@ begin
   end;
 end;
 
-procedure DrawBall(aBall : TBall);
+procedure DrawBall(aBall: TBall);
 begin
   with aBall do
   begin
     SetColor(bc);
-    SetFillStyle(SolidFill, bc);
-    PieSlice(bx, by, 0, 360, br);
+    SetFillStyle(SolidFill,bc);
+    PieSlice(bx,by,0,360,br);
   end;
 end;
 
-procedure InitBalls(w, h : integer);
+procedure InitBalls(w,h: integer);
 var
-  i : integer;
+  i: integer;
 begin
   for i := 1 to nBall do
     with Balls[i] do
     begin
-      bc := random(14) + 1; // color
-      br := random(30) + 20; // radius
-      bx := random(w - br) + br; // start x
-      by := random(h - br) + br; // start y
-      dx := random(9) + 1; // speed x
-      dy := random(9) + 1; // speed y
-      mx := w - br - 1; // max x
-      my := h - br - 1; // max y
-      nx := br + 1; // min x
-      ny := br + 1; // min y
-      gl := random(2) = 0; // move left
-      gu := random(2) = 0; // move up
+      bc := randomColor; // color
+      br := random(34)+23;    // radius
+      bx := random(w-br)+br;  // start x
+      by := random(h-br)+br;  // start y
+      dx := random(13)+1;     // speed x
+      dy := random(13)+1;     // speed y
+      mx := w-br-1;           // max x
+      my := h-br-1;           // max y
+      nx := br+1;             // min x
+      ny := br+1;             // min y
+      gl := random(2) = 0;    // move left
+      gu := random(2) = 0;    // move up
     end;
 end;
 
 var
-  i : integer;
-  mx, my : integer;
+  cs: android_graphics_Color;
+  i,c: integer;
+  mx,my: integer;
 begin
   // enable double buffered
   setBufferEnable(true);
-  InitGraph(detect, 0, '');
+  InitGraph(detect,0,'');
   mx := GetMaxX;
   my := GetMaxY;
-
+  
   // create balls
   Randomize;
-  InitBalls(mx, my);
-  SetLineStyle(SolidLn, 0, ThickWidth);
-
+  InitBalls(mx,my);
+  SetLineStyle(SolidLn,0,ThickWidth);
+  
   // balls animation
   while not keyPressed do
   begin
     // re-initiate if orientation change
-    if (GetMaxX <> mx) or (GetMaxY <> my) then
+    {if (GetMaxX <> mx) or (GetMaxY <> my) then
     begin
       mx := getMaxX;
       my := getMaxY;
-      InitBalls(mx, my);
-    end;
-
-    // clear screen buffer
-    clearBuffer;
+      InitBalls(mx,my);
+    end;}
+    
     // draw background box
     SetColor(15);
-    setFillStyle(solidFill, 0);
-    Bar(0, 0, mx, my);
+    c := cs.argb(100,1,1,1);
+    setFillStyle(solidFill,c);
+    Bar(0,0,mx,my);
     // draw background text
-    SetTextStyle(boldFont, horizDir, 9);
-    SetTextJustify(centerText, centerText);
-    outTextXY(mx div 2, my div 2,
-               '⚽️ Hello 😊');
-    //delay(1);
-
+    SetTextStyle(boldFont,horizDir,9);
+    SetTextJustify(centerText,centerText);
+    outTextXY(mx div 2, my div 2, '⚽️ Hello 😊');
+    
     // draw balls
     for i := 1 to nBall do
       DrawBall(Balls[i]);
     // move balls
     for i := 1 to nBall do
       MoveBall(Balls[i]);
+    
     // redraw buffer to screen
     drawBuffer;
   end;
--- libCompiler/src/main/assets/code_sample/complete_program/text_animate.pas
+++ libCompiler/src/main/assets/code_sample/complete_program/text_animate.pas
@@ -1,6 +1,8 @@
-{ ------——
- Text animation on graphics
- by: Mr Bee -- @pak_lebah
+program animateGraph;
+
+{ --------
+  Text animation on graphics
+  by: Mr Bee -- @pak_lebah
 }
 
 uses CRT, Graph;
@@ -8,81 +10,78 @@ uses CRT, Graph;
 const
    // global value setup
    THE_TEXT = 'PASCAL ON ANDROID';
-   
-   V_MOTION = 20;
-   // motion speed
+   V_MOTION = 20;  // motion speed
    D_MOTION = 1;   // motion delay
 
 type
    // letter structure
-   
    TLetter = record
-      l : string;
-   // character
-      x, y : integer;
-   // position
-      w, h : integer;
-   // dimension
-      c : integer;
-   // color
+      l  : string;  // character
+      x,y: integer; // position
+      w,h: integer; // dimension
+      c  : integer; // color
    end;
-   
    TLetters = array of TLetter;
 
 var
-   goSpread : boolean;
-   // motion direction
-   goStraight : boolean;
-   // animation mode
-   doneX, doneY, done : boolean; // anim state
+   goSpread: boolean; // motion direction
+   goStraight: boolean; // animation mode
+   doneX,doneY,done: boolean; // anim state
+
+// generate random color
+function randomColor: integer;
+var
+   cs: android_graphics_Color;
+begin
+   randomColor :=
+                  cs.argb(255,random(192)+64,
+                            random(192)+64,random(192)+64);
+end;
 
 // spread letters all over the screen
-procedure spread(var l : TLetters);
+procedure spread(var l: TLetters);
 var
-   i : integer;
+   i: integer;
 begin
    for i := 0 to high(l) do
    begin
       // random all over the screen
-      l[i].x := random(getMaxX - l[i].w) + 1;
-      l[i].y := random(getMaxY - l[i].h) + 1;
-      l[i].c := random(getMaxColor) + 9;
+      l[i].x := random(getMaxX-l[i].w)+1;
+      l[i].y := random(getMaxY-l[i].h)+1;
+      l[i].c := randomColor;
    end;
 end;
 
 // gather all letters into a text
-procedure gather(var l : TLetters);
+procedure gather(var l: TLetters);
 var
-   tw, th : integer;
-   // text dimension
-   tx, ty : integer;
-   // text position
-   i : integer;
-   s : string;
+   tw,th: integer; // text dimension
+   tx,ty: integer; // text position
+   i: integer;
+   s: string;
 begin
    // compute text dimension
-   s := '';
-   tw := 0;
+   s := ''; tw := 0;
    for i := 0 to high(l) do s += l[i].l;
    for i := 0 to high(l) do tw += l[i].w;
    th := textHeight(s);
-   tx := random(getMaxX - tw) + 1;
-   ty := random(getMaxY - th) + 1;
+   tx := random(getMaxX-tw)+1;
+   ty := random(getMaxY-th)+1;
    // align text onto new position
    tw := 0;
    for i := 0 to high(l) do
    begin
       l[i].x := tx + tw;
-      l[i].y := ty; // horz aligned
-      l[i].c := random(getMaxColor) + 9;
+      l[i].y := ty;  // horz aligned
+      l[i].c := randomColor;
       tw += l[i].w;
    end;
 end;
 
 // animate on x position
-procedure animX(var f, t : TLetters);
+procedure animX(var f,t: TLetters);
 var
-   i, v : integer;
+   i,v: integer;
 begin
    v := V_MOTION;
    doneX := true;
@@ -110,9 +109,9 @@ begin
 end;
 
 // animate on y position
-procedure animY(var f, t : TLetters);
+procedure animY(var f,t: TLetters);
 var
-   i, v : integer;
+   i,v: integer;
 begin
    v := V_MOTION;
    doneY := true;
@@ -140,94 +139,95 @@ begin
 end;
 
 // animate text
-procedure animate(var f, t : TLetters);
+procedure animate(var f,t: TLetters);
 begin
    if goStraight then
    begin
-      if not doneX then animX(f, t);
-      if not doneY then animY(f, t);
+      if not doneX then animX(f,t);
+      if not doneY then animY(f,t);
    end else
    begin
       if goSpread then
       begin
          // go vertical first
          if not doneY then
-            animY(f, t)
+            animY(f,t)
          else begin
             if not doneX then
-               animX(f, t);
+               animX(f,t);
          end;
       end else
       begin
          // go horizontal first
          if not doneX then
-            animX(f, t)
+            animX(f,t)
          else begin
             if not doneY then
-               animY(f, t);
+               animY(f,t);
          end;
       end;
    end;
-   // state
+   // animation state
    done := doneX and doneY;
 end;
 
 // print letters on the screen
-procedure print(l : TLetters);
+procedure print(l: TLetters);
 var
-   i : integer;
+   i: integer;
 begin
    for i := 0 to high(l) do
    begin
       setColor(l[i].c);
-      outTextXY(l[i].x, l[i].y, l[i].l);
+      outTextXY(l[i].x,l[i].y,l[i].l);
    end;
 end;
 
 // convert string to animated letters
-function toLetters(s : string) : TLetters;
+function toLetters(s: string): TLetters;
 var
-   i, w : integer;
-   l : TLetters;
+   i,w: integer;
+   l: TLetters;
 begin
    w := 1;
-   setLength(l, length(s));
-   for i := 0 to length(s) - 1 do
+   setLength(l,length(s));
+   for i := 0 to length(s)-1 do
    begin
-      l[i].l := s[i + 1];
-      l[i].w := textWidth(l[i].l) + 3;
-      l[i].h := textHeight(l[i].l) + 2;
+      l[i].l := s[i+1];
+      l[i].w := textWidth(l[i].l)+3;
+      l[i].h := textHeight(l[i].l)+2;
       l[i].x := w;
       l[i].y := l[i].h;
-      l[i].c := random(getMaxColor) + 1;
+      l[i].c := randomColor;
       // space doesn't have width
       if l[i].l = ' ' then
-         l[i].w := textWidth('H') + 2;
+         l[i].w := textWidth('H')+2;
       // i is too thin
       if upCase(l[i].l) = 'I' then
-         l[i].w := l[i].w + 2;
+         l[i].w := l[i].w+2;
       w += l[i].w;
    end;
    toLetters := l;
 end;
 
 // remove text footprint
-procedure clear(l : TLetters);
+procedure clear(l: TLetters);
 var
-   i : integer;
+   cs: android_graphics_Color;
+   i: integer;
 begin
    clearBuffer;
    for i := 0 to high(l) do
    begin
-      setColor(-14803426);
-      outTextXY(l[i].x, l[i].y, l[i].l);
+      setColor(cs.rgb(32,32,32));
+      outTextXY(l[i].x,l[i].y,l[i].l);
    end;
 end;
 
 // copy letter colors
-procedure copyCol(var f, t : TLetters);
+procedure copyColor(var f,t: TLetters);
 var
-   i : integer;
+   i: integer;
 begin
    for i := 0 to high(f) do
       t[i].c := f[i].c;
@@ -236,15 +236,15 @@ end;
 // screen setup
 procedure openScreen;
 var
-   gd, gm : integer;
+   gd,gm: integer;
 begin
    randomize;
    // setup graph mode
    gd := detect;
    setBufferEnable(true);
-   initGraph(gd, gm, '');
-   setTextJustify(leftText, bottomText);
-   setTextStyle(gothicFont, horizDir, 3);
+   initGraph(gd,gm,'');
+   setTextJustify(leftText,bottomText);
+   setTextStyle(gothicFont,horizDir,3);
 end;
 
 // screen closing
@@ -256,9 +256,7 @@ end;
 {+++ MAIN PROGRAM +++}
 
 var
-   f, t : TLetters;
-   //c: char = #0;
-
+   f,t: TLetters;
 begin
    // setup
    openScreen;
@@ -281,18 +279,17 @@ begin
       if done then
       begin
          // switch destination
-         copyCol(t, f);
+         copyColor(t,f);
          goSpread := not goSpread;
          if goSpread then spread(t)
          else gather(t);
          
          // freeze for 1 second
          delay(1000);
-         
          // reset state
          doneX := false;
          doneY := false;
-         done := false;
+         done  := false;
          
          // switch mode
          if random(2) = 1 then
@@ -300,7 +297,7 @@ begin
       end;
       
       // motion
-      animate(f, t);
+      animate(f,t);
    until keyPressed;
    
    // close down
